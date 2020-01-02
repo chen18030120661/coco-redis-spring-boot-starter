29c0e284cbe9c99368a2f52abd5176fb36d809a7
chenxinye <chenxinye@miguantech.com>
2020-01-02 11:30:03 +0800

    通过注解自动创建ordinaryQueue队列

--- src/main/java/com/cxy/spring/boot/module/msgqueue/rabbit/ordinary/OrdinaryConfig.java
+++ src/main/java/com/cxy/spring/boot/module/msgqueue/rabbit/ordinary/OrdinaryConfig.java
@@ -1,19 +1,19 @@
-package com.cxy.spring.boot.module.msgqueue.rabbit.ordinary;
-
-import org.springframework.amqp.core.*;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
-
-/**
- * 普通队列
- *
- * @Author xy.chen
- * @Date 2020/1/2
- */
-@Configuration
-public class OrdinaryConfig {
-    @Bean
-    public Queue ordinaryQueue() {
-        return new Queue("ordinaryQueue");
-    }
-}
+//package com.cxy.spring.boot.module.msgqueue.rabbit.ordinary;
+//
+//import org.springframework.amqp.core.*;
+//import org.springframework.context.annotation.Bean;
+//import org.springframework.context.annotation.Configuration;
+//
+///**
+// * 普通队列
+// *
+// * @Author xy.chen
+// * @Date 2020/1/2
+// */
+//@Configuration
+//public class OrdinaryConfig {
+//    @Bean
+//    public Queue ordinaryQueue() {
+//        return new Queue("ordinaryQueue");
+//    }
+//}
--- src/main/java/com/cxy/spring/boot/module/msgqueue/rabbit/ordinary/OrdinaryProcessor.java
+++ src/main/java/com/cxy/spring/boot/module/msgqueue/rabbit/ordinary/OrdinaryProcessor.java
@@ -1,13 +1,16 @@
 package com.cxy.spring.boot.module.msgqueue.rabbit.ordinary;
 
 import org.springframework.amqp.rabbit.annotation.EnableRabbit;
+import org.springframework.amqp.rabbit.annotation.Queue;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
 
 @EnableRabbit
 @Component
 public class OrdinaryProcessor {
-    @RabbitListener(queues="ordinaryQueue")
+//    @RabbitListener(queues="ordinaryQueue")
+    // 通过注解自动创建 spring.simple.queue 队列
+    @RabbitListener(queuesToDeclare = @Queue("ordinaryQueue"))
     public void receiver1(String str){
         System.out.println("Receiver:"+str);
     }
