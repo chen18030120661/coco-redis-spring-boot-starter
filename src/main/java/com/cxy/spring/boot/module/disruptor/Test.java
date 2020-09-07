db5d5edd5b48696e8c1970b8d2a94f62fb09762e
chenxinye <chen123456>
2020-09-07 18:16:21 +0800

    Revert "ceshiaha"

--- src/main/java/com/cxy/spring/boot/module/disruptor/Test.java
+++ src/main/java/com/cxy/spring/boot/module/disruptor/Test.java
@@ -2,6 +2,6 @@ package com.cxy.spring.boot.module.disruptor;
 
 public class Test {
     public static void main(String[] args) {
-        System.out.println("测试EQEQV");
+        System.out.println("测试");
     }
 }
